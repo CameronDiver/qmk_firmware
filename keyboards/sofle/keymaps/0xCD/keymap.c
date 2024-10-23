d6c9a9c6f26b0b310723d417cfcb97b01b8bb8ff
Cameron Diver <camerondiver@gambitresearch.com>
2024-10-23 17:12:07 +0100

    Rotary click to paste

--- keyboards/sofle/keymaps/0xCD/keymap.c
+++ keyboards/sofle/keymaps/0xCD/keymap.c
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | -    |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | \   |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * | \   |   A  |   S  |   D  |   F  |   G  |-------.     ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|  MUTE |    | Paste |------+------+------+------+------+------|
  * |Raise |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Lower |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR |SHIFT | /Space  /       \Enter \  |Bspc | Escape | :   | RGUI |
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_MINS,
   KC_BSLS,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  TL_UPPR,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  TL_LOWR,
+  TL_UPPR,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    KC_PASTE ,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  TL_LOWR,
                  KC_LGUI,KC_LALT,KC_LCTL, KC_LSFT, KC_SPC,      KC_ENT,  KC_BSPC, KC_ESC, KC_COLN, KC_RGUI
 ),
 /* LOWER
