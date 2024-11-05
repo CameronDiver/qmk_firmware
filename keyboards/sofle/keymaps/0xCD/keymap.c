1629417ba43f83ca8dd044bcd072cd6278c25d6b
Cameron Diver <camerondiver@gambitresearch.com>
2024-11-05 20:26:01 +0000

    More stuff

--- keyboards/sofle/keymaps/0xCD/keymap.c
+++ keyboards/sofle/keymaps/0xCD/keymap.c
@@ -4,6 +4,8 @@
 
 #include "quantum/wpm.h"
 
+#include "luna.c"
+
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
@@ -32,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    | Paste |------+------+------+------+------+------|
  * |Raise |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Lower |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |SHIFT | /Space  /       \Enter \  |Bspc | Escape | :   | RGUI |
+ *            | LGui | Raise | Upper |Lctrl | /Space  /       \Enter \  |Bspc | Escape | Lower   | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -41,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_MINS,
   KC_BSLS,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  TL_UPPR,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    KC_PASTE ,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  TL_LOWR,
-                 KC_LGUI,KC_LALT,KC_LCTL, KC_LSFT, KC_SPC,      KC_ENT,  KC_BSPC, KC_ESC, KC_COLN, KC_RGUI
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    KC_PASTE ,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                 KC_LGUI,TL_UPPR, KC_LALT, KC_LCTL, KC_SPC,      KC_ENT,  KC_BSPC, KC_ESC, TL_LOWR, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -67,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                   nn |      |      |      |      |      |      |
+ * |QK_BOOT|      |      |      |      |      |                   |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  | Ins  | Pscr | Menu | Caps | PgUp |                    |      | PWrd |  Up  | NWrd | DLine| Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -80,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT(
-  _______, _______ , _______ , _______ , _______ , _______,             _______,  _______  , _______,  _______ ,  _______ ,_______,
+  QK_BOOT, _______ , _______ , _______ , _______ , _______,             _______,  _______  , _______,  _______ ,  _______ ,_______,
   _______,  KC_INS,  KC_PSCR,   KC_APP,  KC_CAPS, KC_PGUP,              _______, KC_PRVWD,  KC_UP, KC_NXTWD,C(KC_BSPC), KC_BSPC,
   _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_PGDN,              KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_DEL, KC_BSPC,
   _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,  _______,       _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______,
@@ -109,19 +111,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+uint32_t oled_timer = 0;
+
 bool shift_pressed = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        oled_timer = timer_read32();
+    }
     switch (keycode) {
         case KC_LSFT:
             shift_pressed = record->event.pressed;
         break;
         case KC_BSPC:
-            if(shift_pressed) {
-                unregister_code(KC_LSFT);
-                register_code(KC_DELETE);
+            if (shift_pressed) {
+                if(record->event.pressed) {
+                    unregister_code(KC_LSFT);
+                    register_code(KC_DELETE);
+                } else {
+                    unregister_code(KC_DELETE);
+                }
                 return false;
             }
+        break;
 
         case KC_QWERTY:
             if (record->event.pressed) {
@@ -207,20 +219,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    } else {
-        return OLED_ROTATION_180;
-    }
-}
-
-static void render_logo_user(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
-    };
-    oled_write_P(qmk_logo, false);
+    return OLED_ROTATION_270;
 }
 
 void print_status_narrow_user(void) {
@@ -256,10 +255,17 @@ void print_status_narrow_user(void) {
 }
 
 bool oled_task_user(void) {
+    if (last_input_activity_elapsed() > 10000) {
+        oled_off();
+        return false;
+    } else {
+        oled_on();
+    }
+
     if (is_keyboard_master()) {
         print_status_narrow_user();
     } else {
-        render_logo_user();
+        render_luna(1, 1);
     }
     return false;
 }
