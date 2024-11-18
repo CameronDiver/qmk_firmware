a543cddeb772b10d2a475ff1871771befd770626
Cameron Diver <camerondiver@gambitresearch.com>
2024-11-18 09:59:47 +0000

    Use tap dance for square brackets and braces

--- keyboards/sofle/keymaps/0xCD/keymap.c
+++ keyboards/sofle/keymaps/0xCD/keymap.c
@@ -25,6 +25,16 @@ enum custom_keycodes {
     KC_VICTIM
 };
 
+enum {
+    TD_L_SQ_BR,
+    TD_R_SQ_BR,
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_L_SQ_BR] = ACTION_TAP_DANCE_DOUBLE(KC_O, KC_LBRC),
+    [TD_R_SQ_BR] = ACTION_TAP_DANCE_DOUBLE(KC_P, KC_RBRC)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
@@ -44,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_MINS,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    TD(TD_L_SQ_BR)/*KC_O*/,   TD(TD_R_SQ_BR) /*KC_P*/,  KC_MINS,
   KC_BSLS,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    KC_PASTE ,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  SC_RSPC,
                  TL_UPPR, KC_LGUI, KC_LALT, KC_LCTL, KC_SPC,      KC_ENT,  KC_BSPC, KC_ESC, KC_RGUI, TL_LOWR
